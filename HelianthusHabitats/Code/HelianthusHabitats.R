cf658753ab1b1c33f900e111214b5531d5d39745
SamMajumder <sammajumder692@gmail.com>
2023-10-29 22:24:16 -0400

    Helianthus habitats

--- HelianthusHabitats/Code/HelianthusHabitats.R
+++ HelianthusHabitats/Code/HelianthusHabitats.R
@@ -2,9 +2,12 @@ library(rgbif)
 library(here)
 library(tidyverse)
 library(sf)
-library(stars)
+library(RColorBrewer)
+library(cowplot)
+library(gridExtra)
+library(grid)
+library(ggspatial)
 library(rayshader)
-
 
 
 name_backbone("Helianthus")$usageKey
@@ -69,7 +72,8 @@ tidyr::drop_na() %>%
 ######
 
 Sunflower_data <- readRDS(here("Code",
-                               "Sunflower_occurrence_north_america.rds")) 
+                               "Sunflower_occurrence_north_america.rds")) %>%
+                               dplyr::mutate(Species = factor(Species))
 
 ######### 
 ## converting this dataframe into an sf object ####
@@ -82,11 +86,11 @@ Sunflower_data_sf <- sf::st_as_sf(Sunflower_data,
                                   
 
 
-
 ########### importing the Ecoregion shapefile ##
 
-Ecoregion_shape <- sf::st_read(here("Datasets","na_cec_eco_l1",
-                                    "NA_CEC_Eco_Level1.shp"))
+Ecoregion_shape <- sf::st_read(here("Datasets","NA_CEC_Eco_Level3",
+                                    "NA_CEC_Eco_Level3.shp"))
+
 
 
 # Transform Sunflower_sf to match the CRS of Ecoregion_shape
@@ -98,8 +102,8 @@ Sunflower_data_sf <- st_transform(Sunflower_data_sf,
 ### Spatial join with the ecoregion shape file ###
 #### 
 
-Distribution <- Ecoregion_shape %>% 
-                          sf::st_join(Sunflower_data_sf,
+Distribution <- Sunflower_data_sf %>% 
+                          sf::st_join(Ecoregion_shape,
                                       join = st_intersects,
                                       left = FALSE) %>% 
                                        tidyr::drop_na() %>% 
@@ -111,14 +115,134 @@ Distribution <- Ecoregion_shape %>%
 
 
 
+##### Now let's calculate the Centroids of each species ##
+
+Centroids <- Distribution %>%
+                 dplyr::group_by(Species,Ecoregion) %>%
+                 dplyr::summarise(Total = n()) %>%
+                 sf::st_centroid(geometry) %>%
+                 dplyr::arrange(desc(Total)) %>%
+                 ungroup() %>%
+                 dplyr::rename(Centroid_geometry = geometry) %>%
+                 dplyr::mutate(Longitude = sf::st_coordinates(Centroid_geometry)[, 1],
+                               Latitude = sf::st_coordinates(Centroid_geometry)[, 2]) %>%
+                 dplyr::filter(Ecoregion != "WATER")
+                 
 
 
 
 
-ggplot() +
-  geom_sf(data = Distribution) +
-  geom_sf(data = Sunflower_data_sf) +
-  coord_sf(datum = st_crs(Sunflower_data_sf))
+
+### Now filter the original Ecoregion file to only contain the ecoregions we require
+  
+
+#### Let's filter the Ecoregion shape file to only contain the Ecoregions we want ##
+
+Ecoregions <- unique(Centroids$Ecoregion)
+
+Ecoregion_shape <- Ecoregion_shape %>%
+                              dplyr::filter(NA_L1NAME %in% 
+                                              Ecoregions) %>% 
+                              dplyr::rename(Ecoregion = NA_L1NAME)
+
+
+##############
+##### Defining the colors for the legends ####
+############## 
+
+colors_ecoregion <- c("GREAT PLAINS" = "#addd8e", 
+                      "EASTERN TEMPERATE FORESTS" = "#31a354", 
+                      "NORTH AMERICAN DESERTS" = "#e7ba52", 
+                      "MEDITERRANEAN CALIFORNIA" = "#e34a33", 
+                      "TEMPERATE SIERRAS" = "#756bb1",
+                      "NORTHWESTERN FORESTED MOUNTAINS" = "#006d2c", 
+                      "NORTHERN FORESTS" = "#2ca25f", 
+                      "SOUTHERN SEMIARID HIGHLANDS" = "#e7cb94", 
+                      "TROPICAL DRY FORESTS" = "#fd8d3c", 
+                      "TROPICAL WET FORESTS" = "#41ab5d", 
+                      "MARINE WEST COAST FOREST" = "#238b45", 
+                      "TAIGA" = "#3690c0", 
+                      "HUDSON PLAIN" = "#74c476")
+
+
+
+#################
+####
+############ 
+
+species_shapes <- c(1:19, 21:23) 
+
+main_plot <- ggplot() +
+  geom_sf(data = Ecoregion_shape, aes(fill = Ecoregion)) +
+  geom_sf(data = Centroids, aes(shape = Species, fill = Species), size = 3, color = "black", stroke = 1) + 
+  scale_fill_manual(values = c(colors_ecoregion, colors_species)) +
+  scale_shape_manual(values = species_shapes) +
+  coord_sf(datum = st_crs(Sunflower_data_sf)) + 
+  annotation_north_arrow(location = "bl", which_north = "true", 
+                         pad_x = unit(0.0, "in"), pad_y = unit(0.2, "in"),
+                         style = north_arrow_fancy_orienteering) +
+  annotation_scale(location = "bl", width_hint = 0.5) +
+  theme_minimal() +
+  theme(
+    legend.position = "none", 
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_rect(fill = "white"),
+    plot.background = element_blank(),
+    axis.text = element_blank(),
+    axis.title = element_blank(),
+    axis.ticks = element_blank(),
+    panel.border = element_rect(colour = "black", fill = NA, size = 1)
+  )
+
+######################## 
+
+df_ecoregion <- data.frame(Ecoregion = names(colors_ecoregion))
+
+legend_ecoregion_strip <- ggplot(df_ecoregion, aes(x = Ecoregion, y = 1, 
+                                                   fill = Ecoregion)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_fixed(ratio = 0.2) +
+  scale_fill_manual(values = colors_ecoregion) +
+  theme_void() +
+  theme(legend.position="none",
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1,
+                                   size = 8)) 
+
+####################
+
+legend_species_plot <- ggplot(data = data.frame(Species = names(colors_species)), 
+                              aes(x = 1, y = Species, fill = Species, shape = Species)) +
+  geom_point(size = 5, color = "black") + # increased point size and added black outline
+  scale_fill_manual(values = colors_species) +
+  scale_shape_manual(values = species_shapes) +
+  theme_void() +
+  theme(legend.position = c(-0.7, 0.2), 
+        legend.key = element_blank(),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(face = "bold", 
+                                    size = 12),
+        plot.margin = margin(t = 0, r = 0, b = 0, l = -20))
+
+legend_species <- cowplot::get_legend(legend_species_plot) 
+
+########## Combining Everything
+
+final_plot <- grid.arrange(
+  arrangeGrob(main_plot, 
+              legend_species, 
+              ncol = 2, widths = c(4, 1)),
+  legend_ecoregion_strip,
+  ncol = 1, nrow = 2, heights = c(4, 1),
+  top = textGrob("Distribution of a few wild sunflower species in North America", 
+                 gp = gpar(fontface = "bold", fontsize = 20),
+                 just = 0.75)
+)
+
+
+ggsave("sunflower_distribution.png", final_plot, width = 22, height = 10)
+
+
 
 
 
