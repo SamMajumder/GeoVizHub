9feb31a94b7267a6d5766dbcf5f8c9796a1c638c
Sam Majumder <83839244+SamMajumder@users.noreply.github.com>
2023-11-16 00:17:18 -0500

    Add files via upload

---
+++ GiessenRoads/Code/GiessenRoads.R
@@ -0,0 +1,89 @@
+library(osmdata)
+library(sf)
+library(terra)
+library(tidyverse)
+
+##### City boundaries ####
+
+Giessen_boundaries <- osmdata::getbb("Giessen Germany") %>%
+                                  osmdata::opq() %>%
+                                  add_osm_feature(key = 'boundary',
+                                                  value = c('administrative')) %>%
+                      osmdata::osmdata_sf() %>%
+                      .$osm_multipolygons %>%
+                    ### grabbing Giessen ###
+                      dplyr::filter(name == "Gießen")
+
+
+### Main streets 
+
+Streets_Giessen <- osmdata::getbb("Giessen Germany") %>%
+                            opq() %>%
+                    add_osm_feature(key ='highway',
+                                    value = c('motorway','primary',
+                                              'secondary','tertiary')) %>%
+                   osmdata::osmdata_sf() %>% .$osm_lines
+  
+
+### Small streets 
+
+Small_streets_Giessen <- osmdata::getbb("Giessen Germany") %>%
+                               opq() %>%
+                        add_osm_feature(key ='highway',
+                                        value = c('residential', 'living_street',
+                                                  'service', 'footway')) %>%
+                        osmdata::osmdata_sf() %>% .$osm_lines
+
+#### Water 
+
+water <- osmdata::getbb('Giessen Germany') %>%
+                   opq() %>%
+            add_osm_feature(key = 'natural',
+                  value=c('water')) %>%
+            osmdata::osmdata_sf() %>% .$osm_polygons
+
+
+
+p <- Giessen_boundaries %>%
+  ggplot() +
+  geom_sf(fill = "white") +
+  geom_sf(data = Small_streets_Giessen,
+          aes(color = 'Small Streets')) +
+  geom_sf(data = Streets_Giessen,
+          aes(color = 'Main Streets')) +
+  geom_sf(data = water,
+          aes(fill = "Waterways")) +
+  scale_color_manual(
+    values = c('Small Streets' = '#C8A2C8', 'Main Streets' = '#FF7F50'),
+    name = "Street Types",
+    breaks = c('Small Streets', 'Main Streets')
+  ) +
+  scale_fill_manual(
+    values = c('Waterways' = '#87CEEB'),
+    name = "Water Bodies",
+    breaks = c('Waterways')
+  ) +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "black"),
+        plot.background = element_rect(fill = "black"),
+        legend.position = c(0.1, 0.9),
+        plot.caption = element_text(
+          size = 10, color = "white",
+          hjust = 0.98, vjust = 10
+        ),
+        legend.background = element_rect(fill = "black"),
+        legend.text = element_text(color = "white",
+                                   size = 12),
+        legend.title = element_text(color = "white",
+                                    size = 10),
+        plot.title = element_text(hjust = 0.5, vjust = 1, size = 25, color = "white")) +
+  labs(title = "Streets and waterways of Giessen",
+       caption = "Data Source: Map data from OpenStreetMap")
+
+
+ggsave(p,filename = "Giessen_Roads.png",width = 22, height = 10, dpi = 600,
+       bg = "white", device = "png")
+
+
+
+                   
---
+++ GiessenRoads/README.md
@@ -0,0 +1,40 @@
+# Mapping Streets and Waterways of Giessen, Germany
+
+## Introduction
+This project is focused on visualizing the streets and waterways in Giessen, Germany using R. It demonstrates the use of spatial data to create a detailed map, highlighting different types of streets and water bodies. The map aims to provide a clear and informative view of the city's infrastructure.
+
+## Requirements
+To run this project, the following R libraries are needed:
+- `osmdata` - For querying and retrieving spatial data from OpenStreetMap
+- `sf` - For handling and manipulating spatial data
+- `terra` - For spatial data analysis and modeling
+- `tidyverse` - For data manipulation and visualization
+
+## Project Structure
+
+### Retrieving City Boundaries
+- The city boundaries of Giessen, Germany are fetched using the `getbb` function from `osmdata` and filtered for administrative boundaries.
+
+### Mapping Main Streets
+- Main streets including motorways, primary, secondary, and tertiary roads are extracted and processed.
+
+### Mapping Small Streets
+- Small streets such as residential, living streets, service, and footways are also identified and processed.
+
+### Water Bodies Mapping
+- Water bodies within the city limits are retrieved and added to the map.
+
+## Visualization
+- A map is created using `ggplot`:
+  - Different layers are added for small streets, main streets, and water bodies.
+  - Custom color schemes are applied for visual distinction.
+  - Aesthetic enhancements are made for readability and appeal.
+
+## Saving the Final Output
+- The final map is saved as a high-resolution PNG file, ensuring that all details are visible and clear.
+
+## Usage
+To run this project, simply load the R script and execute. Ensure all required libraries are installed.
+
+## Output
+The output of this project is a detailed map titled "Streets and waterways of Giessen", visualizing the city's layout in terms of its roads and water bodies.
